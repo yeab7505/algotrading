bd4c0baab75bcf2e9b5ad8af406eccee2eda5e3d
yeab7505 <yeabsira.tariku-ug@aau.edu.et>
2025-09-12 00:38:34 +0300

    Update ichimoku live.py

--- ichimoku live.py
+++ ichimoku live.py
@@ -53,7 +53,7 @@ TC = 0.0005
 
 ASSETS = ['DOGEUSDT','VETUSDT','HBARUSDT','SANDUSDT','1000PEPEUSDT','1000BONKUSDT','FETUSDT','GRTUSDT','DOTUSDT','LINKUSDT','SUIUSDT']
 MAX_TRENDING_ASSETS = 0  # Maximum number of trending assets to trade
-MAX_CONCURRENT_TRADES = 1  # Set this to the desired number of concurrent trades
+MAX_CONCURRENT_TRADES = 2  # Set this to the desired number of concurrent trades
 active_trades = []
 active_trades_lock = threading.Lock()
 ALLOCATION_MODE = 'dynamic_remaining'  # Options: 'fixed_per_slot', 'dynamic_remaining', 'full_available'
