08275153d8a78a374f616924f0c35aa1d869d445
yeab7505 <yeabsira.tariku-ug@aau.edu.et>
2025-10-29 14:23:25 +0300

    Fix leading span variable names in buy/sell signals

--- ichimoku live.py
+++ ichimoku live.py
@@ -817,7 +817,8 @@ class ForwardIchimokuTrader:
             (current_close > close_t_minus_26)
             and (conversion_line >= base_line)
             
-            and (current_close > leading_Span_A_shifted)
+            and (current_close > leading_Span_A)
+            and (current_close > ledaing_span_B)
             and future_kumo_bullish  # Add future Kumo check
             and (not(pd.isna(psar_long)))  # Only checks if PSAR exists, not its value
             
@@ -828,7 +829,8 @@ class ForwardIchimokuTrader:
             (current_close < close_t_minus_26) 
             and (conversion_line <= base_line) 
             
-            and (current_close < leading_Span_A_shifted)
+            and (current_close < leading_Span_A)
+            and (current_close < leading_span_B)
             and future_kumo_bearish  # Add future Kumo check
             and (not(pd.isna(psar_short)))
               # Only checks if PSAR exists, not its value
