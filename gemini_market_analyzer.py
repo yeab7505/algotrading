3871ce76093f7f99fc2d0432346f1fd3f338740e
yeab7505 <yeabsira.tariku-ug@aau.edu.et>
2025-11-21 11:18:47 +0300

    Update gemini_market_analyzer.py

--- gemini_market_analyzer.py
+++ gemini_market_analyzer.py
@@ -720,5 +720,5 @@ def check_market_consolidation(df: pd.DataFrame, symbol: str,
     Returns:
         bool: True if market is consolidating
     """
-    analyzer = GeminiMarketAnalyzer(api_key='AIzaSyAdyo9u1iFoyoqSXCG3h38ADtuZHmc85vg')
+    analyzer = GeminiMarketAnalyzer(api_key='AIzaSyByp6gMvJjyVpYCceTTLdbjTUQUiAv66sk')
     return analyzer.is_market_consolidating(df, symbol)
